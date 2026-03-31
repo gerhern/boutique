ac17512e4ddbcea3f1d2f7b33420f433ccfcfdc9
Carmine <carmine112255@gmail.com>
2026-03-31 20:21:53 +0200

    Laravel spatie mail preview installed

--- config/mail.php
+++ config/mail.php
@@ -48,6 +48,9 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
+        'preview' => [
+            'transport' => 'preview'
+        ],
 
         'ses' => [
             'transport' => 'ses',
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            {{-- Texto en lugar de imagen para evitar bloqueos de carga --}}
+            <span style="font-size: 18px; font-weight: 700; letter-spacing: 2px; text-transform: uppercase; color: #D4AF37;">
+                {{ config('app.name') }}
+            </span>
+        </a>
+    </td>
+</tr>
